c4f258cf4a3b6cad74916b8afd5537d7d86a4010
Raman Maksimčuk <dotnet044@gmail.com>
2019-06-03 19:31:51 +0300

    [BT] Move TodoList example to root React component

--- basic-tutorial/src/components/App.js
+++ basic-tutorial/src/components/App.js
@@ -3,7 +3,13 @@ import Footer from './Footer';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodoList from '../containers/VisibleTodoList';
 
-class App extends React.Component {
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { todoApp as rootReducer } from '../reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+export class App extends React.Component {
     render () {
         return (
             <div>
@@ -15,4 +21,21 @@ class App extends React.Component {
     }
 }
 
-export default App;
+export const run = () => {
+    const appName = 'Redux Basics. Todos App';
+
+    console.log(`
+#------------------------
+# ${appName}
+#------------------------
+`);
+
+    const store = createStore(rootReducer, devToolsEnhancer({ name: appName }));
+
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        document.getElementById('root')
+    );
+};
--- basic-tutorial/src/index.js
+++ basic-tutorial/src/index.js
@@ -1,23 +1,7 @@
-// import * as reducers from './reducers'
-// import * as dataFlow from './dataFlow'
+import * as reducers from './reducers';
+import * as dataFlow from './dataFlow';
+import * as app from './components/App';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { todoApp as rootReducer } from './reducers';
-import App from './components/App';
-
-// reducers.run()
-// dataFlow.run()
-
-// --------------------------------------
-
-const store = createStore(rootReducer);
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
+reducers.run();
+dataFlow.run();
+app.run();
